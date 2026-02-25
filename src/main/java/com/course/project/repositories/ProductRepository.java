002c6264acf1f16f20700ed7eddfe43c00ec1ffd
WeberLima <barbosadelimawerner@gmail.com>
2026-02-25 19:07:09 -0300

    Products

---
+++ src/main/java/com/course/project/repositories/ProductRepository.java
@@ -0,0 +1,9 @@
+package com.course.project.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.course.project.entities.Product;
+
+public interface ProductRepository extends JpaRepository<Product, Long>{
+
+}
---
+++ src/main/java/com/course/project/services/ProductService.java
@@ -0,0 +1,28 @@
+package com.course.project.services;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.course.project.entities.Product;
+import com.course.project.repositories.ProductRepository;
+
+
+@Service
+public class ProductService {
+
+		@Autowired
+		private ProductRepository repository;
+		
+		
+		public List<Product> findAll(){
+			return repository.findAll();
+		}
+		
+		public Product findById(Long id) {
+			Optional<Product> obj = repository.findById(id);
+			return obj.get();
+		}
+}
