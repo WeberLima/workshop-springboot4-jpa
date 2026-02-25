533598378d320d59614341aedf45de7ec4aa7138
WeberLima <barbosadelimawerner@gmail.com>
2026-02-25 18:50:31 -0300

    Category

---
+++ src/main/java/com/course/project/services/CategoryService.java
@@ -0,0 +1,28 @@
+package com.course.project.services;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.course.project.entities.Category;
+import com.course.project.repositories.CategoryRepository;
+
+
+@Service
+public class CategoryService {
+
+		@Autowired
+		private CategoryRepository repository;
+		
+		
+		public List<Category> findAll(){
+			return repository.findAll();
+		}
+		
+		public Category findById(Long id) {
+			Optional<Category> obj = repository.findById(id);
+			return obj.get();
+		}
+}
